954e7784f60ba8dd1e966ccdf8797f9ed98a8798
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-07-24 17:43:52 +0500

    :

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,7 @@ const config = {
   REDIS_PORT: process.env.REDIS_PORT,
   SESSION_TYPE: process.env.SESSION_TYPE,
   DOMAIN: process.env.DOMAIN,
-  PORT: process.env.PORT,
+  PORT: process.env.DOMAIN_PORT,
   MONGO_URL: process.env.MONGO_URL,
   URL: process.env.URL,
 };
