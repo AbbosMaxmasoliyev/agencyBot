7e10882afb6c4aa5690b1d4b40d08e2ad5914a1d
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-07-25 18:54:01 +0500

    Change corsOptions

--- index.js
+++ index.js
@@ -36,25 +36,22 @@ mongoose.connect(mongoDB, {
     console.error("Failed to connect to MongoDB", err);
     process.exit(1);
 });
-const allowedOrigins = [
-    'https://blogerwebapp.vercel.app',
-    'https://bloger-agency-adminka.vercel.app'
-];
+
+
+
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: [
+        'https://blogerwebapp.vercel.app',
+        'https://bloger-agency-adminka.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
+
+
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
