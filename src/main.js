97dd613c86f8e9a02fbd144a254ccf10adc612ec
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-09-16 10:01:06 +0500

    Pisheded

--- src/main.js
+++ src/main.js
@@ -1,47 +1,100 @@
 require("dotenv").config();
-
-const { bot } = require("./core/bot");
-const session = require("./core/session");
-const stage = require("./scenes");
 const User = require("../models/user");
-
-const i18next = require('../i18');
+const { Scenes, Markup } = require("telegraf");
+const { default: axios } = require("axios");
 const logger = require("../utils/logger");
 
+const scene = new Scenes.BaseScene("start");
 
-bot.use(async (ctx, next) => {
-  let user = await User.findOne({ userId: ctx.from?.id })
-  const language = user?.language || "uz"; // Foydalanuvchi tilini aniqlash
-  ctx.i18n = i18next.cloneInstance();
-  ctx.i18n.changeLanguage(language);
-  await next();
-});
+let BOT_TOKEN = process.env.BOT_TOKEN;
+let WEB_APP_URL = process.env.WEB_APP;
 
+async function safeReply(ctx, message, extra = {}) {
+  try {
+    await ctx.reply(message, extra);
+  } catch (error) {
+    logger.error(`Error sending message to user ${ctx.chat.id}: ${error.message}`);
+  }
+}
 
-bot.use(session);
-bot.use(stage.middleware());
+async function safeSendMessage(chat_id, message, extra = {}) {
+  try {
+    await axios.post(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+      chat_id,
+      text: message,
+      ...extra,
+    });
+  } catch (error) {
+    logger.error(`Error sending message via axios to chat_id ${chat_id}: ${error.message}`);
+  }
+}
 
-let main = bot.start(async (ctx) => {
-  const userId = ctx.from.id;
-
+scene.enter(async (ctx) => {
+  let userId = ctx.message.chat.id;
   let user = await User.findOne({ userId, active: true });
-  logger.info(user)
-  if (!user) {
-    user = new User({ userId, active: true, status: false });
-    await user.save();
+  logger.info(user);
+
+  // Set language if available
+  if (user?.language) {
+    await ctx.i18n.changeLanguage(user.language);
   }
 
-  if (!user.firstName || !user.lastName || !user.phoneNumber) {
-    ctx.scene.enter("language");
+  console.log(userId);
+
+  if (user?.web_app?.gender) {
+    const keyboard = Markup.inlineKeyboard([
+      [
+        {
+          text: ctx.i18n.t('open_web_app'),
+          web_app: { url: `${WEB_APP_URL}/user/${userId}` },
+        },
+      ],
+    ]).resize();
+
+    const menuButton = Markup.inlineKeyboard([
+      [
+        Markup.button.webApp(ctx.i18n.t('open_web_app'), `${WEB_APP_URL}/user/${userId}`),
+      ],
+    ]);
+
+    // Notify a specific user
+    await safeSendMessage("1094968462", `@${ctx.chat.username}`);
+
+    // Send reply to the current user
+    await safeReply(ctx, ctx.i18n.t('welcome'), {
+      reply_markup: {
+        keyboard: [
+          [
+            { text: ctx.i18n.t('open_web_app'), web_app: { url: `${WEB_APP_URL}/user/${userId}` } }
+          ]
+        ],
+        inline_keyboard: [
+          [
+            {
+              text: ctx.i18n.t('open_web_app'),
+              web_app: { url: `${WEB_APP_URL}/user/${userId}` },
+            }
+          ]
+        ],
+        resize_keyboard: true
+      }
+    });
   } else {
-    ctx.scene.enter("start");
+    let keyboard = Markup.inlineKeyboard([
+      [
+        {
+          text: ctx.i18n.t("registr"),
+          web_app: { url: `${WEB_APP_URL}/user/${userId}/bot` },
+        },
+      ],
+    ]).resize();
+
+    console.log("salom");
+
+    // Send registration prompt if the user isn't registered
+    await safeReply(ctx, ctx.i18n.t("registration"), keyboard);
   }
 });
 
+module.exports = scene;
 
-
-
-
-
-
-module.exports = main
--- src/scenes/auth.js
+++ src/scenes/auth.js
@@ -15,16 +15,23 @@ function validatePhoneNumber(phoneNumber) {
   return phoneRegex.test(phoneNumber);
 }
 
+async function safeReply(ctx, message, extra = {}) {
+  try {
+    await ctx.reply(message, extra);
+  } catch (error) {
+    return logger.error(`Error sending message to user ${ctx.chat.id}: ${error.message}`);
+  }
+}
+
 const scene = new WizardScene(
   "auth",
   async (ctx) => {
-    ctx.reply(ctx.i18n.t("name"));
-    let userId = ctx.chat.id
-    let user = await User.findOne({ userId, active: true, status: false })
+    await safeReply(ctx, ctx.i18n.t("name"));
+    let userId = ctx.chat.id;
+    let user = await User.findOne({ userId, active: true, status: false });
     console.log("====24");
     console.log(user);
     console.log("====24");
-
 
     return ctx.wizard.next();
   },
@@ -32,43 +39,42 @@ const scene = new WizardScene(
     const firstName = ctx.message?.text;
 
     if (!validateName(firstName)) {
-      ctx.reply(ctx.i18n.t("error_name"));
+      await safeReply(ctx, ctx.i18n.t("error_name"));
       return ctx.wizard.selectStep(ctx.wizard.cursor);
     }
 
     ctx.wizard.state.firstName = firstName;
-    ctx.reply(ctx.i18n.t("surname"));
-    logger.info("ctx language=> " + ctx.i18n.language)
+    await safeReply(ctx, ctx.i18n.t("surname"));
+    logger.info("ctx language=> " + ctx.i18n.language);
     return ctx.wizard.next();
   },
   async (ctx) => {
     const lastName = ctx.message?.text;
 
     if (!validateName(lastName)) {
-      ctx.reply(ctx.i18n.t("error_surname"));
+      await safeReply(ctx, ctx.i18n.t("error_surname"));
       return ctx.wizard.selectStep(ctx.wizard.cursor);
     }
 
     ctx.wizard.state.lastName = lastName;
-    ctx.reply(ctx.i18n.t("phoneNumber"), {
+    await safeReply(ctx, ctx.i18n.t("phoneNumber"), {
       reply_markup: {
         keyboard: [
           [
             {
-              text: ctx.i18n.t("Share Phone Number"), // Masalan: "Telefon raqamimni ulashish"
-              request_contact: true // Telefon raqamni so'rash
-            }
-          ]
+              text: ctx.i18n.t("Share Phone Number"),
+              request_contact: true,
+            },
+          ],
         ],
-        resize_keyboard: true, // Klaviatura o'lchamini avtomatik moslashtirish
-        one_time_keyboard: false // Klaviaturani faqat bir marta ko'rsatish
-      }
+        resize_keyboard: true,
+        one_time_keyboard: false,
+      },
     });
 
     return ctx.wizard.next();
   },
   async (ctx) => {
-
     const phoneNumber = ctx.message?.contact?.phone_number;
 
     console.log(phoneNumber);
@@ -77,34 +83,38 @@ const scene = new WizardScene(
       ctx.wizard.state.phoneNumber = phoneNumber;
       const userId = ctx.from.id.toString();
 
-      await User.findOneAndUpdate(
-        { userId, active: true },
-        {
-          $set: {
-            firstName: ctx.wizard.state.firstName,
-            lastName: ctx.wizard.state.lastName,
-            phoneNumber: ctx.wizard.state.phoneNumber,
-            language: ctx.wizard.state.language,
-            fromTelegram: ctx.from,
-            web_app: { userTelegramId: `${ctx.chat.username}-${Date.now()}-${ctx.chat.id}` }
+      try {
+        await User.findOneAndUpdate(
+          { userId, active: true },
+          {
+            $set: {
+              firstName: ctx.wizard.state.firstName,
+              lastName: ctx.wizard.state.lastName,
+              phoneNumber: ctx.wizard.state.phoneNumber,
+              language: ctx.wizard.state.language,
+              fromTelegram: ctx.from,
+              web_app: {
+                userTelegramId: `${ctx.chat.username}-${Date.now()}-${ctx.chat.id}`,
+              },
+            },
           },
-        },
-        { new: true }
-      );
+          { new: true }
+        );
 
-      ctx.reply(ctx.i18n.t("saved_success"), {
-        reply_markup: {
-          remove_keyboard: true
-        }
-      });
-      return ctx.scene.enter("start");
+        await safeReply(ctx, ctx.i18n.t("saved_success"), {
+          reply_markup: {
+            remove_keyboard: true,
+          },
+        });
+        return ctx.scene.enter("start");
+      } catch (error) {
+        logger.error(`Error updating user ${userId}: ${error.message}`);
+      }
     } else {
-      ctx.reply(ctx.i18n.t("phoneNumber_error"));
-
+      await safeReply(ctx, ctx.i18n.t("phoneNumber_error"));
       ctx.wizard.selectStep(ctx.wizard.cursor);
     }
   }
-
 );
 
 module.exports = scene;
